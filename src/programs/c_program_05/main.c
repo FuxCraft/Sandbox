1f353d1d6cbebd68d8dc092ad32291b1c970f04e
FuxCraft <145333403+FuxCraft@users.noreply.github.com>
2025-03-26 07:47:46 +0000

    bogogogoggoog

--- src/programs/c_program_05/main.c
+++ src/programs/c_program_05/main.c
@@ -2,52 +2,80 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define ARR_SIZE 3
+#define ARR_SIZE 10
 
 void shuffle(int *array)
 {
-    if (ARR_SIZE > 1) 
+  if (ARR_SIZE > 1)
+  {
+    size_t i;
+    for (i = 0; i < ARR_SIZE - 1; i++)
     {
-        size_t i;
-        for (i = 0; i < ARR_SIZE - 1; i++) 
-        {
-          size_t j = i + rand() / (RAND_MAX / (ARR_SIZE - i) + 1);
-          int t = array[j];
-          array[j] = array[i];
-          array[i] = t;
-        }
+      size_t j = i + rand() / (RAND_MAX / (ARR_SIZE - i) + 1);
+      int t = array[j];
+      array[j] = array[i];
+      array[i] = t;
     }
+  }
 }
-int main(int argc, char **argv) {
-  
+
+int arraySortedOrNot(int arr[], int n)
+{
+  // Array has one or no element
+  if (n == 0 || n == 1)
+    return 1;
+
+  for (int i = 1; i < n; i++)
+  {
+    // Unsorted pair found
+    if (arr[i - 1] > arr[i])
+      return 0;
+  }
+
+  // No unsorted pair found
+  return 1;
+}
+
+int main(int argc, char **argv)
+{
+
   int arr[ARR_SIZE];
 
   srand(time(0));
 
-  for(int i = 0; i < ARR_SIZE; i++){
-    arr[i] = rand() % 10000;
+  for (int i = 0; i < ARR_SIZE; i++)
+  {
+    arr[i] = rand() % 10;
   }
 
-for(int j = 0; j < 1;){
-  for(int i = 0; i < ARR_SIZE; i++){
-    printf("%d; ", arr[i]);
-  }
-  shuffle(arr);
-  printf("\n");
-  for(int i = 0; i < ARR_SIZE-1; i++){
-    if(arr[i] > arr[i+1]){
-      break;
-    } else if(i == ARR_SIZE-1){
-      j++;
+  for (int j = 0; j < 1; j++)
+  {
+    for (int k = 0; k < ARR_SIZE; k++)
+    {
+      printf("%d; ", arr[k]);
+    }
+    shuffle(arr);
+    printf("\n");
+    if (arraySortedOrNot(arr, ARR_SIZE))
+    {
+      for (int i = 0; i < ARR_SIZE; i++)
+      {
+        printf("%d; ", arr[i]);
+      }
+      return 0;
+    }
+    else
+    {
+      j--;
     }
   }
-}
 
-for(int i = 0; i < ARR_SIZE; i++){
-  printf("%d; ", arr[i]);
-}
-printf("\n");
+  for (int i = 0; i < ARR_SIZE; i++)
+  {
+    printf("%d; ", arr[i]);
+  }
+  printf("\n");
 
-printf("ARR sorted");
+  printf("ARR sorted");
   return 0;
 }
